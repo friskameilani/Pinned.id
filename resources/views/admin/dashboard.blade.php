fb2248f7e50fae1e778abeb700485a34aeba069d
ervanoyy <ervanoyy@gmail.com>
2020-12-13 21:08:29 +0700

    add new card on dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -7,57 +7,107 @@
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <section class="content-header" style="height: 100px;">
-      <h1 class="text-left" style="padding-left: 100px;">
+      <h1 class="text-left" style="padding-left: 30px;">
         Dashboard
       </h1>
     </section>
   </div>
 
-    <main>
+
       <!-- Main content -->
       <section class="content" style="height: 200px;">
         <!-- Small boxes (Stat box) -->
-        <div class="row">
-          <div class="card offset-lg-3" style="min-width: 250px; max-height: 100px;">
+        <div class="row offset-md-1">
+          <!-- CARD 1 -->
+          <div class="card offset-md-1" style="width: 250px; max-height: 100px;">
             <div class="row no-gutters">
-              <div class="col-md-4 bg-secondary" style="padding-left: 23px; padding-top:20px; max-height: 100px; font-size: 40px;">
-                <i class="text-center fa fa-user"></i>
+              <div class="col-md-4 bg-secondary" style="padding-left: 19px; padding-top:20px; max-height: 100px; font-size: 40px;">
+                <i class="text-center fa fa-eye"></i>
               </div>
               <div class="col-md-8">
                 <div class="card-body">
-                  <h3 class="card-title">{{$total_tailor}}</h3>
-                  <p class="card-text">Penjahit Terdaftar</p>
+                  <h3 class="card-title text-height-2">5</h3>
+                  <p class="card-text text-height-15">Jumlah Pengunjung</p>
                 </div>
               </div>
             </div>
           </div>
-
-        <!-- Small boxes (Stat box) -->
-        <div class="row">
-          <div class="card offset-lg-9" style="min-width: 250px; max-height: 100px;">
+        
+          <!-- CARD 2 -->
+          <div class="card offset-md-1" style="min-width: 250px; max-height: 100px;">
             <div class="row no-gutters">
               <div class="col-md-4 bg-primary" style="padding-left: 23px; padding-top: 22px; max-height: 100px; font-size: 40px;">
                 <i class="text-center fa fa-tag"></i>
               </div>
               <div class="col-md-8">
                 <div class="card-body">
-                  <h3 class="card-title">{{$total_product}}</h3>
+                  <h3 class="card-title text-height-2">{{$total_product}}</h3>
                   <p class="card-text">Produk Terdaftar</p>
                 </div>
               </div>
             </div>
           </div>
+
+          <!-- CARD 3 -->
+          <div class="card offset-md-1" style="min-width: 250px; max-height: 100px;">
+            <div class="row no-gutters">
+              <div class="col-md-4 bg-danger" style="padding-left: 23px; padding-top: 22px; max-height: 100px; font-size: 40px;">
+                <i class="text-center fa fa-user"></i>
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h3 class="card-title text-height-2">{{$total_tailor}}</h3>
+                  <p class="card-text">Penjahit Terdaftar</p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
+
+        <div class="row offset-md-1" style="padding-top:20px; padding-bottom: 20px">
+          <!-- CARD 4 -->
+          <div class="card offset-md-1" style="min-width: 250px; max-height: 100px;">
+            <div class="row no-gutters">
+              <div class="col-md-4 bg-warning" style="padding-left: 23px; padding-top:20px; max-height: 100px; font-size: 40px;">
+                <i class="text-center fa fa-check"></i>
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h3 class="card-title text-height-2">5</h3>
+                  <p class="card-text text-height-15">Pesanan Terkonfirmasi</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        
+          <!-- CARD 5 -->
+          <div class="card offset-md-1" style="min-width: 250px; max-height: 100px;">
+            <div class="row no-gutters">
+              <div class="col-md-4 bg-success" style="padding-left: 18px; padding-top: 22px; max-height: 100px; font-size: 40px;">
+                <i class="text-center fa fa-cart-arrow-down"></i>
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h3 class="card-title text-height-2">11</h3>
+                  <p class="card-text">Total Pesanan</p>
+                </div>
+              </div>
+            </div>
+          </div>
+      </div>
       </section>                       
-    </main>
+
     
 
     <!-- Chart -->
-    <figure class="highcharts-figure">
-    <div id="chartorderan"></div>
-    <p class="highcharts-description">
-    </p>
-  </figure>
+      <br></br>
+    
+      <figure class="highcharts-figure">
+        <div id="chartorderan"></div>
+        <p class="highcharts-description">
+        </p>
+      </figure>
+    
 <!-- ./wrapper -->
 
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
@@ -129,7 +179,7 @@ responsive: {
         chartOptions: {
             legend: {
                 layout: 'horizontal',
-                align: 'right',
+                align: 'left',
                 verticalAlign: 'bottom'
             }
         }
