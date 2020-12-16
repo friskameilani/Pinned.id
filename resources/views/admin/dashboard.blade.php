eed9bcee7c9ecdfce47aef87b8b108578b85e152
ervanoyy <ervanoyy@gmail.com>
2020-12-16 17:44:38 +0700

    update dashboard; remove chart

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -19,77 +19,77 @@
         <!-- Small boxes (Stat box) -->
         <div class="row offset-md-1">
           <!-- CARD 1 -->
-          <div class="card offset-md-1" style="width: 250px; max-height: 100px;">
+          <div class="card" style="width: 300px; height: 150px;">
             <div class="row no-gutters">
-              <div class="col-md-4 bg-secondary" style="padding-left: 23px; padding-top:20px; max-height: 100px; font-size: 40px;">
+              <div class="col-md-5 bg-secondary d-flex justify-content-center" style="padding-top:47px; height: 150px; font-size: 60px;">
                 <i class="text-center fa fa-eye"></i>
               </div>
-              <div class="col-md-8">
-                <div class="card-body">
-                  <h3 class="card-title text-height-2">{{$total_user}}</h3>
-                  <p class="card-text text-height-15">Jumlah Akun Terdaftar</p>
+              <div class="col-md-7">
+                <div class="card-body" style="padding-top: 35px;">
+                  <h1 class="card-title text-height-3">{{$total_user}}</h1>
+                  <p class="card-text text-height-2" style="font-size: 16px;">Jumlah Akun Terdaftar</p>
                 </div>
               </div>
             </div>
           </div>
         
           <!-- CARD 2 -->
-          <div class="card offset-md-1" style="min-width: 250px; max-height: 100px;">
+          <div class="card offset-md-1" style="width: 300px; height: 150px;">
             <div class="row no-gutters">
-              <div class="col-md-4 bg-primary" style="padding-left: 23px; padding-top: 22px; max-height: 100px; font-size: 40px;">
+              <div class="col-md-5 bg-primary d-flex justify-content-center" style="padding-top:48px; height: 150px; font-size: 60px;">
                 <i class="text-center fa fa-tag"></i>
               </div>
-              <div class="col-md-8">
-                <div class="card-body">
-                  <h3 class="card-title text-height-2">{{$total_product}}</h3>
-                  <p class="card-text">Produk Terdaftar</p>
+              <div class="col-md-7">
+                <div class="card-body" style="padding-top: 35px;">
+                  <h1 class="card-title text-height-3">{{$total_product}}</h1>
+                  <p class="card-text-height-2" style="font-size: 16px;">Produk Terdaftar</p>
                 </div>
               </div>
             </div>
           </div>
 
           <!-- CARD 3 -->
-          <div class="card offset-md-1" style="min-width: 250px; max-height: 100px;">
+          <div class="card offset-md-1" style="width: 300px; height: 150px;">
             <div class="row no-gutters">
-              <div class="col-md-4 bg-danger" style="padding-left: 23px; padding-top: 22px; max-height: 100px; font-size: 40px;">
+              <div class="col-md-5 bg-danger d-flex justify-content-center" style="padding-top:42px; height: 150px; font-size: 60px;">
                 <i class="text-center fa fa-user"></i>
               </div>
-              <div class="col-md-8">
-                <div class="card-body">
-                  <h3 class="card-title text-height-2">{{$total_tailor}}</h3>
-                  <p class="card-text">Penjahit Terdaftar</p>
+              <div class="col-md-7">
+                <div class="card-body" style="padding-top: 35px;">
+                  <h1 class="card-title text-height-3">{{$total_tailor}}</h1>
+                  <p class="card-text-height-2" style="font-size: 16px;">Penjahit Terdaftar</p>
                 </div>
               </div>
             </div>
           </div>
         </div>
 
-        <div class="row offset-md-1" style="padding-top:20px; padding-bottom: 20px">
+        <div class="row offset-md-1" style="padding-top:40px; padding-bottom: 20px">
           <!-- CARD 4 -->
-          <div class="card offset-md-1" style="max-width: 250px; max-height: 100px;">
+          <div class="card" style="width: 300px; height: 150px;">
             <div class="row no-gutters">
-              <div class="col-md-4 bg-warning" style="padding-left: 23px; padding-top:20px; max-height: 100px; font-size: 40px;">
+              <div class="col-md-5 bg-warning d-flex justify-content-center" style="padding-top:45px; height: 150px; font-size: 60px;">
                 <i class="text-center fa fa-check"></i>
               </div>
-              <div class="col-md-8">
-                <div class="card-body">
-                  <h3 class="card-title text-height-2">{{$confirmed_order}}</h3>
-                  <p class="card-text text-height-15">Pesanan Terkonfirmasi</p>
+              <div class="col-md-7">
+                <div class="card-body" style="padding-top: 35px;">
+                  <h1 class="card-title text-height-3">{{$confirmed_order}}</h1>
+                  <p class="card-text-height-2" style="font-size: 16px;">Pesanan Terkonfirmasi</p>
                 </div>
               </div>
             </div>
           </div>
         
           <!-- CARD 5 -->
-          <div class="card offset-md-1" style="min-width: 250px; max-height: 100px;">
+          <div class="card offset-md-1" style="width: 300px; height: 150px;">
             <div class="row no-gutters">
-              <div class="col-md-4 bg-success" style="padding-left: 18px; padding-top: 22px; max-height: 100px; font-size: 40px;">
+              <div class="col-md-5 bg-success d-flex justify-content-center" style="padding-top:45px; height: 150px; font-size: 60px;">
                 <i class="text-center fa fa-cart-arrow-down"></i>
               </div>
-              <div class="col-md-8">
-                <div class="card-body">
-                  <h3 class="card-title text-height-2">{{$total_order}}</h3>
-                  <p class="card-text">Total Pesanan</p>
+              <div class="col-md-7">
+                <div class="card-body" style="padding-top: 35px;">
+                  <h1 class="card-title text-height-3">{{$total_order}}</h1>
+                  <p class="card-text-height-2" style="font-size: 16px;">Total Pesanan</p>
                 </div>
               </div>
             </div>
@@ -97,97 +97,11 @@
         <div> 
       </div>
       </section>                       
-
-    
-
-    <!-- Chart -->
-      <br></br>
-    
-      <figure class="highcharts-figure">
-        <div id="chartorderan"></div>
-        <p class="highcharts-description">
-        </p>
-      </figure>
-    
 <!-- ./wrapper -->
 
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
   $.widget.bridge('uibutton', $.ui.button);
-</script>
-
-<!-- CHART SCRIPT -->
-<script src="https://code.highcharts.com/highcharts.js"></script>
-<script src="https://code.highcharts.com/modules/series-label.js"></script>
-<script src="https://code.highcharts.com/modules/exporting.js"></script>
-<script src="https://code.highcharts.com/modules/export-data.js"></script>
-<script src="https://code.highcharts.com/modules/accessibility.js"></script>
-
-<script>
-Highcharts.chart('chartorderan', {
-
-title: {
-    text: 'Order Chart'
-},
-
-subtitle: {
-    text: 'Agustus-Desember 2020 (dalam lusin)'
-},
-
-yAxis: {
-    title: {
-        text: 'Jumlah'
-    }
-},
-
-xAxis: {
-    accessibility: {
-        rangeDescription: 'Range: 2020 to 2025'
-    }
-},
-
-legend: {
-    layout: 'vertical',
-    align: 'right',
-    verticalAlign: 'middle'
-},
-
-plotOptions: {
-    series: {
-        label: {
-            connectorAllowed: false
-        },
-        pointStart: 8
-    }
-},
-
-series: [{
-    name: 'Kaos',
-    data: [23, 34, 26, 60, 44]
-}, {
-    name: 'Kemeja',
-    data: [10, 15, 22, 25, 19]
-}, {
-    name: 'Tie Dye Shirt',
-    data: [22, 45, 30, 39, 50]
-}],
-
-responsive: {
-    rules: [{
-        condition: {
-            maxWidth: 500
-        },
-        chartOptions: {
-            legend: {
-                layout: 'horizontal',
-                align: 'left',
-                verticalAlign: 'bottom'
-            }
-        }
-    }]
-}
-
-});
 </script>
 
 @endsection
